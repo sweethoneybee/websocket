830b7b4e8ab387831a1ada0777a50aaf1eb3dbba
jsjphone8 <jsjphone8@gmail.com>
2020-07-12 17:42:51 +0900

    Simple two way chat program

---
+++ client.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#define PORT 9999
+
+void sendMsg(const int &);
+void recvMsg(const int &);
+
+int main(void)
+{
+  int client_sock;
+  struct sockaddr_in server_addr;
+
+  const char *msg = "hello! world!";
+  int recv_len;
+
+  if ((client_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket error");
+    return 1;
+  }
+
+  memset(&server_addr, 0x00, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_addr.sin_port = htons(PORT);
+
+  if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+    perror("connect error!");
+    return 1;
+  }
+
+  std::thread t1(sendMsg, client_sock);
+  std::thread t2(recvMsg, client_sock);
+
+  t1.join();
+  t2.join();
+
+  close(client_sock);
+  return 0;
+}
+
+void sendMsg(const int &client_sock)
+{
+  std::string inputMsg;
+  char sendBuffer[1024];
+
+  memset(sendBuffer, 0, sizeof(char) * 1024);
+  printf("enter the msg>> ");
+
+  while (inputMsg.compare("exit") != 0)
+  {
+    std::getline(std::cin, inputMsg);
+    int strLen = inputMsg.length() >= 1024 ? 1023 : inputMsg.length();
+
+    for (int i = 0; i < strLen; i++)
+      sendBuffer[i] = inputMsg[i];
+    sendBuffer[strlen(sendBuffer)] = '\0';
+
+    if (send(client_sock, sendBuffer, strlen(sendBuffer) + 1, 0) < 0)
+    {
+      printf("server closed!\n");
+      return;
+    }
+    memset(sendBuffer, 0, sizeof(char) * 1024);
+  }
+  return;
+}
+
+void recvMsg(const int &client_sock)
+{
+  int recv_len;
+  char recvBuffer[1024];
+  memset(recvBuffer, 0, 1024);
+
+  while ((recv_len = recv(client_sock, recvBuffer, 1024, 0)) > 0)
+  {
+    recvBuffer[recv_len] = '\0';
+    printf("\nmsg from server: %s", recvBuffer);
+    memset(recvBuffer, 0, sizeof(char) * 1024);
+  }
+
+  return;
+}
---
+++ server.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#define PORT 9999
+
+void recvMsg(const int &);
+void sendMsg(const int &);
+
+int main(void)
+{
+  int server_sock, client_sock;
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[1024];
+  int len, addr_len, recv_len;
+
+  // TCP/IP에서는 SOCK_STREAM을 UDP/IP에서는 SOCK_DGRAM을 사용함
+  // server socket 생성
+  if ((server_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket error");
+    return 1;
+  }
+
+  // server socket에 대한 설정
+  memset(&server_addr, 0x00, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;                // IPv4 protocol
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // accept all 32bit IPv4 ip addresses
+  server_addr.sin_port = htons(PORT);              // use PORT 9999
+
+  // 커널에 server_sock 등록해서 사용할 수 있게 하기
+  if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+    perror("bind error");
+    return 1;
+  }
+
+  if (listen(server_sock, 5) < 0)
+  {
+    perror("listen error");
+    return 1;
+  }
+
+  addr_len = sizeof(client_addr);
+
+  printf("wating for client...\n");
+
+  if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, (socklen_t *)&addr_len)) < 0)
+  {
+    perror("connect error!\n");
+    return 1;
+  }
+  printf("client ip: %s\n", inet_ntoa(client_addr.sin_addr));
+
+  std::thread t1(recvMsg, client_sock);
+  std::thread t2(sendMsg, client_sock);
+
+  t1.join();
+  t2.join();
+
+  close(client_sock);
+  close(server_sock);
+  return 0;
+}
+
+void sendMsg(const int &client_sock)
+{
+  std::string inputMsg;
+  char sendBuffer[1024];
+
+  memset(sendBuffer, 0, sizeof(char) * 1024);
+  printf("enter the msg>> ");
+
+  while (inputMsg.compare("exit") != 0)
+  {
+    std::getline(std::cin, inputMsg);
+    int strLen = inputMsg.length() >= 1024 ? 1023 : inputMsg.length();
+
+    for (int i = 0; i < strLen; i++)
+      sendBuffer[i] = inputMsg[i];
+    sendBuffer[strlen(sendBuffer)] = '\0';
+
+    if (send(client_sock, sendBuffer, strlen(sendBuffer) + 1, 0) < 0)
+    {
+      printf("server closed!\n");
+      return;
+    }
+    memset(sendBuffer, 0, sizeof(char) * 1024);
+  }
+  return;
+}
+
+void recvMsg(const int &client_sock)
+{
+  int recv_len;
+  char buffer[1024];
+
+  while ((recv_len = recv(client_sock, buffer, 1024, 0)) > 0)
+  {
+    // 안전을 위해 받는 쪽, 보내는 쪽 둘다 \0 처리를 해줌.
+    buffer[recv_len] = '\0';
+    if (strcmp(buffer, "exit") == 0)
+    {
+      printf("disconnect...\n");
+      break;
+    }
+    printf("msg from client: %s\n", buffer);
+    send(client_sock, buffer, strlen(buffer) + 1, 0);
+    memset(buffer, 0, 1024);
+  }
+
+  return;
+}
